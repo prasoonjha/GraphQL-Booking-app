94adfbd3af3b9d1ee809e305708d468621b75776
Prasoon Jha <prasoonjha69@gmail.com>
2020-06-13 19:04:31 +0530

    test query and mutation added

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphql = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.user = bodyParser.json();
+
+app.use(
+  "/graphql",
+  graphql({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String 
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }    
+    `),
+    rootValue: {
+      events: () => {
+        return ["Cooking", "Sailing", "Rock Climbing", "All-night-coding"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
 
 app.listen(5000, () => {
   console.log("Server started on port 5000");
